e95d565db7f1f523205bf860fe4c5ecdbc113f03
Loïk <loguima@gmail.com>
2020-10-18 17:46:39 +0200

    Add images loading, improve extract

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from common.constantes import *
 from extract.extract_main import ExtractMain
+from load.loaddata import LoadData
+from load.loadimages import LoadImages
 from transform.transform import Transform
-from load.load import Load
 
 if __name__ == '__main__':
     ExtractMain(URL_TO_SCRAP, EXTRACTED_DIR)
     Transform(EXTRACTED_DIR, TRANSFORMED_DIR)
-    Load(TRANSFORMED_DIR, LOADED_DIR)
+    LoadData(TRANSFORMED_DIR, LOADED_DIR)
+
+    LoadImages(TRANSFORMED_DIR, IMAGES_DIR)
